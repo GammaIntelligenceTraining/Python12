17cbefa7feb78b8525a78c30a14fc0f30e61ffdf
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-06-06 12:50:01 +0300

    List, tuple, set

--- 101_homeworks/002_strings_and_methods/homework.py
+++ 101_homeworks/002_strings_and_methods/homework.py
@@ -1,8 +1,6 @@
 # Write a code to return "Hero" from given string
 example_string1 = "Hello bro"
-hero_string = example_string1[0:2] + example_string1[-2:]
-print(hero_string)
-
+print(example_string1[:2] + example_string1[-2:])
 
 # Write a code to return "Jack is my name"
 example_string2 = "jack Is My NAME"
@@ -11,11 +9,11 @@ print(example_string2.capitalize())
 # Write a code to return "Get rid of stars please"
 example_string3 = "*Get rid of stars please*"
 print(example_string3.strip('*'))
-print(example_string3.replace('*', ''))
 
 
 # Write a code to return "Hello my name is Jack"
-example_string4 = "hello my naMe is jack"
+example_string4 = "hello my name is jack"
+print(example_string4[:-4].capitalize() + example_string4[-4:].capitalize())
 print(example_string4.capitalize().replace('jack', 'Jack'))
 
 
@@ -23,21 +21,18 @@ print(example_string4.capitalize().replace('jack', 'Jack'))
 var1 = "jack"
 var2 = "hello"
 var3 = "MY NAME IS"
-
-var1 = var1.capitalize()
-var2 = var2.capitalize()
-var3 = var3.lower()
-
-print(f'{var2}, {var3} {var1}')
-
-print(f'{var2.capitalize()}, {var3.lower()} {var1.capitalize()}')
-
+print(var2.capitalize() + ', ' + var3.lower() + ' ' + var1.capitalize())
 
 
 # Write a code to return byte_string text value
-# 'utf-8' 'utf-16'
 byte_string = b"\316\273"
-print(byte_string.decode('utf8'))
+print(byte_string.decode('utf-8'))
 
-
+# Write a code to return True if cost is greater than 500$
+example_string5 = "It cost me 1000.00$"
+cost = float(example_string5[-8:-1])
+if cost > 500:
+    print(True)
+else:
+    print(False)
 
